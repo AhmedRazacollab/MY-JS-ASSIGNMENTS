fd382398d8f2a7f750b7f4407d8ed1271a573a7d
Ahmed Raza <ahmedshaikh0314@gmail.com>
2025-05-24 19:43:49 +0500

    Add files via upload

---
+++ Assignment-06/script.js
@@ -0,0 +1,27 @@
+let heading = document.getElementById('heading-1');
+heading.style.color = 'blue' ;
+
+
+let input = document.getElementById('input-1');
+input.style.backgroundColor = 'lightgray';
+input.style.border = '2px solid blue';
+input.style.padding = '10px';
+input.style.borderRadius = '5px';
+
+
+
+let button = document.getElementById('button-1');
+button.style.backgroundColor = 'yellow';
+button.style.border = '2px solid blue';
+button.style.padding = '10px';
+button.style.borderRadius = '5px';
+
+let button2 = document.getElementsByClassName('body-btn')[0];
+button2.style.padding = '10px';
+button2.style.borderRadius = '5px';
+button2.style.border = '2px solid blue';
+button2.style.backgroundColor = 'yellow';
+let body = document.body;
+body.style.backgroundColor = 'lightblue';
+
+
